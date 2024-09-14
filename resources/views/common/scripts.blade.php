c373986a0edeeb053ed2aa0498492e0899170a8b
rguri <gurpreetjalalana11@gmail.com>
2024-09-14 10:31:55 +0530

    test ignore

--- resources/views/common/scripts.blade.php
+++ resources/views/common/scripts.blade.php
@@ -6,14 +6,14 @@
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{url('public/lib/wow/wow.min.js')}}"></script>
-    <script src="{{url('public/lib/easing/easing.min.js')}}"></script>
-    <script src="{{url('public/lib/waypoints/waypoints.min.js')}}"></script>
-    <script src="{{url('public/lib/counterup/counterup.min.js')}}"></script>
-    <script src="{{url('public/lib/owlcarousel/owl.carousel.min.js')}}"></script>
+    <script src="{{url('lib/wow/wow.min.js')}}"></script>
+    <script src="{{url('lib/easing/easing.min.js')}}"></script>
+    <script src="{{url('lib/waypoints/waypoints.min.js')}}"></script>
+    <script src="{{url('lib/counterup/counterup.min.js')}}"></script>
+    <script src="{{url('lib/owlcarousel/owl.carousel.min.js')}}"></script>
    
     <!-- Template Javascript -->
-    <script src="{{url('public/js/main.js')}}"></script>
+    <script src="{{url('js/main.js')}}"></script>
     </body>
 <!-- Full-Screen Loader -->
 
