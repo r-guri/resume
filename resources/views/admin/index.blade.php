602d3890853aefb69d10c508e4cbf8c4c9094778
rguri <gurpreetjalalana11@gmail.com>
2024-09-24 22:25:01 +0530

    re

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,17 +1,18 @@
 @include ('admin.css')
 
   <body  class=" d-flex flex-column">
-  <div class="page page-center">
-      <div class="container container-tight py-4">
-        <div class="text-center mb-4">
-            
-        <a href="." class="navbar-brand navbar-brand-autodark"><h1>99CodeHub</h1></a>
-        
-        </div>
-        <div class="card card-md">
-          <div class="card-body">
-            <h2 class="h2 text-center mb-4">Login to your account</h2>
-            <form action="{!! url('login') !!}" method="POST" autocomplete="off" novalidate> @if ($errors->has('email'))
+    <div class="page page-center">
+      <div class="container container-normal py-4">
+        <div class="row align-items-center g-4">
+          <div class="col-lg">
+            <div class="container-tight">
+              <div class="text-center mb-4">
+                <a href="." class="navbar-brand navbar-brand-autodark"><h1>99CodeHub</h1></a>
+              </div>
+              <div class="card card-md">
+                <div class="card-body">
+                  <h2 class="h2 text-center mb-4">Login to your account</h2>
+                  <form action="{!! url('login') !!}" method="POST" autocomplete="off" novalidate> @if ($errors->has('email'))
                                    <div class="alert alert-danger" role="alert">
                     {!! $errors->first('email') !!}
                     </div>
@@ -41,7 +42,7 @@
                 @endif
                 @csrf <!-- This is important for Laravel to verify the form submission -->
                
-                <div class="mb-3">
+                    <div class="mb-3">
                       <label class="form-label">Email address</label>
                       <input type="email" name="email" class="form-control" placeholder="your@email.com" autocomplete="off">
                     </div>
@@ -60,8 +61,8 @@
                           </a>
                         </span>
                       </div>
-              </div>
-              <div class="mb-2">
+                    </div>
+                    <div class="mb-2">
                       <label class="form-check">
                         <input type="checkbox" class="form-check-input"/>
                         <span class="form-check-label">Remember me on this device</span>
@@ -70,23 +71,18 @@
                     <div class="form-footer">
                       <button type="submit" class="btn btn-primary w-100">Sign in</button>
                     </div>
-</form>
-          </div>
-          <div class="hr-text">or</div>
-          <div class="card-body">
-            <div class="row">
-              <div class="col"><a href="{{route('auth.google')}}" class="btn w-100">
-                  <!-- Download SVG icon from http://tabler-icons.io/i/brand-github -->
-                  <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-brand-google"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20.945 11a9 9 0 1 1 -3.284 -5.997l-2.655 2.392a5.5 5.5 0 1 0 2.119 6.605h-4.125v-3h7.945z" /></svg>   Login with Google
-                </a></div>
-              
+                  </form>
+                </div>
+              </div>
+              <div class="text-center text-secondary mt-3">
+                Don't have account yet? <a href="{{url('signup')}}" tabindex="-1">Sign Up</a>
+              </div>
             </div>
           </div>
+          <div class="col-lg d-none d-lg-block">
+            <img src="public/admin/static/illustrations/undraw_secure_login_pdn4.svg" height="300" class="d-block mx-auto" alt="">
+          </div>
         </div>
-        <div class="text-center text-secondary mt-3">
-                Don't have account yet? <a href="{{url('signup')}}" tabindex="-1"><b>New Register</b></a>
-              </div>
       </div>
     </div>
-  
 @include ('admin.scripts')
