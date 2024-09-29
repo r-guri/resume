53b830b7bb40ad0df847c4858540a1d4aed08efe
rguri <gurpreetjalalana11@gmail.com>
2024-09-29 10:29:53 +0530

    css change

--- resources/views/admin/scripts.blade.php
+++ resources/views/admin/scripts.blade.php
@@ -1,11 +1,11 @@
   <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{url('lib/wow/wow.min.js')}}"></script>
-    <script src="{{url('lib/easing/easing.min.js')}}"></script>
-    <script src="{{url('lib/waypoints/waypoints.min.js')}}"></script>
-    <script src="{{url('lib/counterup/counterup.min.js')}}"></script>
-    <script src="{{url('lib/owlcarousel/owl.carousel.min.js')}}"></script>
-    <script src="{{url('lib/js/main.js')}}"></script>
+    <script src="{{url('public/lib/wow/wow.min.js')}}"></script>
+    <script src="{{url('public/lib/easing/easing.min.js')}}"></script>
+    <script src="{{url('public/lib/waypoints/waypoints.min.js')}}"></script>
+    <script src="{{url('public/lib/counterup/counterup.min.js')}}"></script>
+    <script src="{{url('public/lib/owlcarousel/owl.carousel.min.js')}}"></script>
+    <script src="{{url('public/lib/js/main.js')}}"></script>
     
 </body>
 </html>
